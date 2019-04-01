50f8a9ab3021287de0cb3892543dfe5e52425029
Rei Omori <47578679+reiomori@users.noreply.github.com>
2019-04-01 10:46:16 +0900

    Update test_slam.sh

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -5,6 +5,4 @@ xterm  -e  "rosrun gmapping slam_gmapping" &
 sleep 5
 xterm  -e  "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm  -e  "roslaunch turtlebot_teleop keyboard_teleop.launch" & 
-sleep 5
-xterm  -e  "rosrun rviz rviz" 
+xterm  -e  "roslaunch turtlebot_teleop keyboard_teleop.launch"
