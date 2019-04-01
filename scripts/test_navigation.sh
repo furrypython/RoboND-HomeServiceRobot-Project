fa8e926dcd1d22a4d1210b0c8c8159472e9c2540
Rei Omori <47578679+reiomori@users.noreply.github.com>
2019-04-01 11:50:23 +0900

    Update test_navigation.sh

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
-xterm  -e  "rosrun gmapping slam_gmapping" &
+xterm  -e  "rosrun turtlebot_gazebo amcl_demo.launch" &
 sleep 5
-xterm  -e  "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-sleep 5
-xterm  -e  "roslaunch turtlebot_teleop keyboard_teleop.launch"
+xterm  -e  "roslaunch turtlebot_rviz_launchers view_navigation.launch"
