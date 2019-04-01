a8329f87318e2953aa5de9217b5837481c2399c7
Rei Omori <47578679+reiomori@users.noreply.github.com>
2019-04-01 10:37:42 +0900

    Update test_slam.sh

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -2,10 +2,10 @@
 #!/bin/sh
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
-xterm  -e  " roslaunch " & 
+xterm  -e  " rosrun gmapping gmapping_demo.launch " &
 sleep 5
-xterm  -e  " rosrun rviz rviz" &
+xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch " &
 sleep 5
-xterm  -e  " rosrun rviz rviz"  &
+xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch " & 
 sleep 5
 xterm  -e  " rosrun rviz rviz" 
