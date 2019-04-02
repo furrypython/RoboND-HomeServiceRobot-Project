adf61c849048da0c15b7f382dbed281500eac38b
Rei Omori <47578679+reiomori@users.noreply.github.com>
2019-04-02 09:04:43 +0900

    Update add_markers.cpp

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -16,7 +16,7 @@ int main( int argc, char** argv )
   {
     visualization_msgs::Marker marker;
     // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-    marker.header.frame_id = "map";
+    marker.header.frame_id = "/my_frame";
     marker.header.stamp = ros::Time::now();
 
     // Set the namespace and id for this marker.  This serves to create a unique ID
