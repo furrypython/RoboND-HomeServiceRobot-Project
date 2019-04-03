64354142ee322bfab51a51518ba84e0117a6413f
Rei Omori <47578679+reiomori@users.noreply.github.com>
2019-04-03 15:39:13 +0900

    Update test_navigation.sh

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-xterm  -e  "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+xterm  -e  "cd $(pwd)/../..; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/../map/building.world" &
 sleep 5
-xterm  -e  "rosrun turtlebot_gazebo amcl_demo.launch" &
+xterm  -e  "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../map/building.yaml" &
 sleep 5
 xterm  -e  "roslaunch turtlebot_rviz_launchers view_navigation.launch"
