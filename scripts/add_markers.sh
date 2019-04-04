63685244b5c3a7e6d9423505879cdbc2f10b94c6
Rei Omori <47578679+reiomori@users.noreply.github.com>
2019-04-04 21:14:58 +0900

    Delete add_markers.sh

--- scripts/add_markers.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
-sleep 5
-xterm -e "rosrun turtlebot_gazebo amcl_demo.launch" &
-sleep 5
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-sleep 5
-xterm -e "roslaunch add_markers add_markers_node"
