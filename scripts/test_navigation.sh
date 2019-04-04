5f29d166eca70e58efb3261437cfb541263d5b9f
Rei Omori <47578679+reiomori@users.noreply.github.com>
2019-04-04 21:15:09 +0900

    Delete test_navigation.sh

--- scripts/test_navigation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xterm  -e  "cd $(pwd)/../..; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(pwd)/../map/building.world" &
-sleep 5
-xterm  -e  "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(pwd)/../map/building.yaml" &
-sleep 5
-xterm  -e  "roslaunch turtlebot_rviz_launchers view_navigation.launch"
