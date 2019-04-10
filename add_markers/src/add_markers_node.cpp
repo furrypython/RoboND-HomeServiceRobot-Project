fee3df26550adfc0d02cdc63e2b1921119cf2056
Rei Omori <47578679+reiomori@users.noreply.github.com>
2019-04-10 11:53:50 +0900

    Update add_markers_node.cpp

--- add_markers/src/add_markers_node.cpp
+++ add_markers/src/add_markers_node.cpp
@@ -2,90 +2,98 @@
 #include <visualization_msgs/Marker.h>
 #include <nav_msgs/Odometry.h>
 
-class addMarkers{
+class addMarkers {
 public:
-  addMarkers(){
-    //Topic to publish
-    marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
-    // Subscribe to odom topic
-    odom_pub = n.subscribe("odom", 10, addMarkers::odomCallback, this);
-  }
+    addMarkers() {
+        //Topic to publish
+        marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+        // Subscribe to odom topic
+        odom_pub = n.subscribe("odom", 10, addMarkers::odomCallback, this);
+    }
   
-  void setPose(visualization_msgs::Marker& marker, double posX,double posY, double oriW) {
-    // Set the pose of the marker
-    marker.pose.position.x = poseX;
-    marker.pose.position.y = poseY;
-    marker.pose.orientation.w = oriW;
-  }  
+    void setPose(double posX,double posY){
+        // Set the pose of the marker
+        marker.pose.position.x = posX;
+        marker.pose.position.y = posY;
+        marker.pose.position.z = 0.0
+        marker.pose.orientation.x = 0.0;
+        marker.pose.orientation.y = 0.0;
+        marker.pose.orientation.z = 0.0;
+        marker.pose.orientation.w = 1.0;
+    }
   
-  void setMarker(visualization_msgs::Marker& marker){
-    // Set the initial shape type to be a cube
-    uint32_t shape = visualization_msgs::Marker::CUBE;
+    void publishMarker(){
+        // Sleep till a subscriber is ready and then publish the marker.
+        while (marker_pub.getNumSubscribers() < 1){
+            ROS_WARN_ONCE("Please create a subscriber to the marker");
+            sleep(1);
+        }
+        marker_pub.publish(marker);
+    }
+  
+    void setMarker(){
+        // Set the initial shape type to be a cube
+        uint32_t shape = visualization_msgs::Marker::CUBE;
 
-    // Set the marker type.
-    marker.type = shape;
-
-    // Set the frame ID and timestamp.
-    marker.header.frame_id = "map";
-    marker.header.stamp = ros::Time::now();
-
-    // Set the namespace and id for this marker.
-    marker.ns = "add_markers_ns";
-    marker.id = 0;
+        // Set the marker type.
+        marker.type = shape;
     
-    // Set the pose of the marker.
-    setPose()
+        // Set the marker action. 
+        marker.action = visualization_msgs::Marker::ADD;
+    
+        // Set the frame ID and timestamp
+        marker.header.frame_id = "map";
+        marker.header.stamp = ros::Time::now();
 
-    // Set the scale of the marker
-    marker.scale.x = 0.2;
-    marker.scale.y = 0.2;
-    marker.scale.z = 0.2;
+        // Set the namespace and id for this marker
+        marker.ns = "add_markers_ns";
+        marker.id = 0;
+    
+        // Initially show the marker at the pickup zone
+        setPose(pickup[0], pickup[1]);
 
-    // Set the color
-    marker.color.r = 0.913f;
-    marker.color.g = 1.0f;
-    marker.color.b = 0.0f;
-    marker.color.a = 1.0;
+        // Set the scale of the marker
+        marker.scale.x = 0.2;
+        marker.scale.y = 0.2;
+        marker.scale.z = 0.2;
 
-    marker.lifetime = ros::Duration();
-  }
+        // Set the color
+        marker.color.r = 0.913f;
+        marker.color.g = 1.0f;
+        marker.color.b = 0.0f;
+        marker.color.a = 1.0;
 
- void waitSub(ros::Publisher& marker_pub){
-  // Sleep until a subscriber is ready
-    while (marker_pub.getNumSubscribers() < 1)
-    {
-      ROS_WARN_ONCE("Please create a subscriber to the marker");
-      sleep(1);
-    }
-  }
-  
-
-  
-  void odomCallback(const nav_msgs::Odometry& odom){
-    double robotPoseX = odom.pose.position.x;
-    double robotPoseY = odom.pose.position.y;
-    double diff = 0.00001;
-  
-    double dpx = abs(robotPoseX - pickup.x);
-    double dpy = abs(robotPoseY - pickup.y);
-    if(!reachPickup && dpx < diff && dpy < diff){
-      ROS_INFO("Reached the pickup zone");
-      reachPickup = true;
+        marker.lifetime = ros::Duration();
+        publishMarker();
     }
   
-    double ddx = abs(robotPoseX - dropoff.x);
-    double ddy = abs(robotPoseY - dropoff.y);
-    if(!reachDropoff && ddx < diff && ddy < diff){
-      ROS_INFO("Reached the dropoff zone");
-      reachDropoff = true;
-    }  
-  }
+    void odomCallback(const nav_msgs::Odometry& odom){
+        double robotPosX = odom.pose.position.x;
+        double robotPosY = odom.pose.position.y;
+        double diff = 0.00001;
+  
+        double dpx = abs(robotPoseX - pickup.x);
+        double dpy = abs(robotPoseY - pickup.y);
+        if(!reachPickup && dpx < diff && dpy < diff){
+            ROS_INFO("Reached the pickup zone");
+            reachPickup = true;
+        }
+  
+        double ddx = abs(robotPoseX - dropoff.x);
+        double ddy = abs(robotPoseY - dropoff.y);
+        if(!reachDropoff && ddx < diff && ddy < diff){
+            ROS_INFO("Reached the dropoff zone");
+            reachDropoff = true;
+        }  
+    }
   
 private:
   ros::NodeHandle n;
   ros::Publisher marker_pub;
   ros::Subscriber odom_pub;
   visualization_msgs::Marker marker;
+  double pickup[2] = {-0.488, -4.103};
+  double dropoff[2] = {5.281, -2.463};
   
   bool reachPickup = false;
   bool reachDropoff = false;
@@ -96,17 +104,10 @@ int main(int argc, char** argv ){
   ros::init(argc, argv, "add_markers");
   ros::Rate r(1);
 
-  if (!ros::ok()) {
-    return 0;
-  }
+  // Initially show the marker at the pickup zone
+  addMarkers::setMarker();
 
-  addMarkers::setMarker(marker);
-  addMarkers::waitSub(marker_pub);
-
-  // -----------------------------------------------------------
-  // Publish the marker at the pickup zone
-  addMarkers::setPose(marker, -0.488, -4.103, 1.0);
-  marker.action = visualization_msgs::Marker::ADD;
+  
   marker_pub.publish(marker);
 
   // Check if reach the pickup zone
